75967faa04c3e063658ee8f0766ae27ffd34271c
j-novaes <joaovictor.nb21@hotmail.com>
2022-03-05 12:22:31 +0000

    Edited App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+//Components
+import CurrencyBox from './components/CurrencyBox/CurrencyBox';
+import CurrencyInput from './components/CurrencyInput/CurrencyInput';
+
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [baseCurrency, setBaseCurrency] = useState('BRL');
+
   return (
     <div>
-      
+      <CurrencyInput
+        setAmount={setAmount}
+        setBaseCurrency={setBaseCurrency}
+      />
+      <CurrencyBox
+        amount={amount}
+        baseCurrency={baseCurrency}
+        initialCurrency='BRL'
+      />
+
+      <CurrencyBox
+        amount={amount}
+        baseCurrency={baseCurrency}
+        initialCurrency='EUR'
+      />
+
+      <CurrencyBox
+        amount={amount}
+        baseCurrency={baseCurrency}
+        initialCurrency='USD'
+      />
+
+      <CurrencyBox
+        amount={amount}
+        baseCurrency={baseCurrency}
+        initialCurrency='BRL'
+      />
     </div>
   );
 }
